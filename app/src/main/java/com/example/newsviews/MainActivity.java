4c0fb14ad202806308afeb0b90c962e4413e16c5
Shohanur Rahman <shohanurvu@gmail.com>
2019-02-02 10:18:07 +0600

    Commit1

--- app/src/main/java/com/example/newsviews/MainActivity.java
+++ app/src/main/java/com/example/newsviews/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.newsviews;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,6 +10,8 @@ import android.support.v7.widget.Toolbar;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +21,22 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar=findViewById(R.id.toolbar);
        setSupportActionBar(toolbar);
 
+        drawer=findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+         toggle.syncState();
+
 
     }
 
-
-
-
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else
+        {
+        super.onBackPressed();}
+    }
 }
